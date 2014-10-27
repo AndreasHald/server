3cc78183ae1875a1cfebd857cf7655421c50be23
MikkelFrid <mikkel.frid@live.dk>
2014-10-27 13:50:43 +0100

    Rensning af server
    Der er lavet en helt ny server og taget backup af den gamle.

--- src/TCPServer - old.java
+++ src/TCPServer - old.java
@@ -1,32 +1,5 @@
 import java.io.*;
 import java.net.*;
-
-class TCPServer
-{
-   public static void main(String argv[]) throws Exception
-      {
-         String clientSentence;
-         String capitalizedSentence;
-         ServerSocket welcomeSocket = new ServerSocket(6789);
-
-         while(true)
-         {
-            Socket connectionSocket = welcomeSocket.accept();
-            BufferedReader inFromClient =
-               new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            clientSentence = inFromClient.readLine();
-            System.out.println("Received: " + clientSentence);
-            capitalizedSentence = clientSentence.toUpperCase() + '\n';
-            outToClient.writeBytes(capitalizedSentence);
-         }
-      }
-}
-
-
-/*
-import java.io.*;
-import java.net.*;
 import JsonClasses.CalendarInfo;
 
 import com.google.gson.stream.JsonReader;
@@ -72,6 +45,5 @@ class TCPServer {
 			//BufferedWriter writer = new BufferedWriter(arg0)
 			}
 		}
-	} 
-
-*/
+	
+		} 
