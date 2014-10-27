d1955d0a06c33bfd64ff4d5c0b264fea6c2aae40
AndreasHald <andreas.boris.hald@gmail.com>
2014-10-27 14:06:16 +0100

    TCP Server & Encryption ready to edit
    Skal rettes til

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -5,9 +5,12 @@ class TCPServer
 {
    public static void main(String argv[]) throws Exception
       {
-         String clientSentence;
-         String capitalizedSentence;
-         ServerSocket welcomeSocket = new ServerSocket(6789);
+	    GiantSwitch GS = new GiantSwitch();
+		encryption cryp = new encryption();
+	   
+        String recieved;
+        String reply;
+        ServerSocket welcomeSocket = new ServerSocket(6789);
 
          while(true)
          {
@@ -15,10 +18,17 @@ class TCPServer
             BufferedReader inFromClient =
                new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            clientSentence = inFromClient.readLine();
-            System.out.println("Received: " + clientSentence);
-            capitalizedSentence = clientSentence.toUpperCase() + '\n';
-            outToClient.writeBytes(capitalizedSentence);
+            recieved = inFromClient.readLine();
+            
+            
+            System.out.println("Received: " + recieved);
+            // SVAR MODTAGET
+            
+            //cryp.decrypt(recieved);
+            
+            
+            //reply = recieved.toUpperCase() + '\n';
+            //outToClient.writeBytes(reply);
          }
       }
 }
